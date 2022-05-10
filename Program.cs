1b363c30b4990101a0a05249d57fa997681724a7
Alex_Fike <alexfike2016@gmail.com>
2022-05-10 14:58:38 +0500

    solved task

--- Program.cs
+++ Program.cs
@@ -9,7 +9,28 @@ void PrintArray(string[] arg)
     Console.WriteLine();
 }
 
+(int, string[]) CountingElementsNewArray(string[] arg)
+{
+    int count = 0;
+    for (int i = 0; i < arg.Length; i++)
+    {
+        if (arg[i].Length < 4) count++;
+    }
+    return (count, arg);
+}
 
-
+string[] ThreeCharactersArray((int count, string[] array) arg)
+{
+    string[] newArray = new string[arg.count];
+    for (int i = 0, j = 0; i < arg.array.Length; i++)
+    {
+        if (arg.array[i].Length < 4)
+        {
+            newArray[j++] = arg.array[i];
+        }
+    }
+    return newArray;
+}
 
 PrintArray(array);
+PrintArray(ThreeCharactersArray(CountingElementsNewArray(array)));
