f9355909dd405adec07019b4bb686d2b21ba47ff
Alex_Fike <alexfike2016@gmail.com>
2022-05-10 14:56:04 +0500

    array and print array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "213f", "234", "gbff", "gpw", "22", "t", "-341" };
+
+void PrintArray(string[] arg)
+{
+    for (int i = 0; i < arg.Length; i++)
+    {
+        Console.Write($"{arg[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+PrintArray(array);
